9105ac830cd331753945fb9d51ff37c58a6208e5
Pal32003 <pal32003@163.com>
2023-05-17 09:14:54 +0800

    cm test

---
+++ cli/inc/cli_minus.h
@@ -0,0 +1,6 @@
+#ifndef __CLI_MINUS_H__
+#define __CLI_MINUS_H__
+
+int cliMinus(int a, int b);
+
+#endif
---
+++ cli/src/cli_minus.c
@@ -0,0 +1,6 @@
+#include "../inc/cli_minus.h"
+
+int cliMinus(int a, int b)
+{
+	return a-b;
+}
